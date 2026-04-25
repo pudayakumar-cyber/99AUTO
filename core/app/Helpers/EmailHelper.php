47d958d58eb404a12c17c7798fc919740a951ad4
alinaeembaig <ali_naeem89@live.com>
2026-04-26 01:32:12 +0500

    Harden order email placeholders and logging

--- core/app/Helpers/EmailHelper.php
+++ core/app/Helpers/EmailHelper.php
@@ -8,9 +8,11 @@ namespace App\Helpers;
 
 use App\{
     Models\EmailTemplate,
+    Models\Order,
     Models\Setting
 };
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Session;
 use PHPMailer\PHPMailer\{
     PHPMailer,
@@ -50,10 +52,7 @@ class EmailHelper
     {
         $template = EmailTemplate::whereType($emailData['type'])->first();
         try {
-            $email_body = preg_replace("/{user_name}/", $emailData['user_name'], $template->body);
-            $email_body = preg_replace("/{order_cost}/", $emailData['order_cost'], $email_body);
-            $email_body = preg_replace("/{transaction_number}/", $emailData['transaction_number'], $email_body);
-            $email_body = preg_replace("/{site_title}/", $this->setting->title, $email_body);
+            $email_body = $this->replaceTemplateTokens($template->body, $emailData);
 
             $this->mail->setFrom($this->setting->email_from, $this->setting->email_from_name);
             $this->mail->addAddress($emailData['to']);
@@ -61,11 +60,19 @@ class EmailHelper
             $this->mail->Subject = $template->subject;
             $this->mail->Body = $email_body;
             $this->mail->send();
+            Log::info('Template email sent.', $this->buildEmailLogContext($emailData, [
+                'template_type' => $emailData['type'] ?? null,
+                'recipient' => $emailData['to'] ?? null,
+            ]));
             if ($this->setting->order_mail == 1) {
                 $this->adminMail($emailData);
             }
         } catch (Exception $e) {
-            // dd($e->getMessage());
+            Log::error('Template email send failed.', $this->buildEmailLogContext($emailData, [
+                'template_type' => $emailData['type'] ?? null,
+                'recipient' => $emailData['to'] ?? null,
+                'error' => $e->getMessage(),
+            ]));
         }
 
         return true;
@@ -109,22 +116,55 @@ class EmailHelper
         try {
 
             $template = EmailTemplate::whereType('New Order Admin')->first();
-            $email_body = preg_replace("/{user_name}/", $emailData['user_name'], $template->body);
-            $email_body = preg_replace("/{order_cost}/", $emailData['order_cost'], $email_body);
-            $email_body = preg_replace("/{transaction_number}/", $emailData['transaction_number'], $email_body);
-            $email_body = preg_replace("/{site_title}/", $this->setting->title, $email_body);
+            $email_body = $this->replaceTemplateTokens($template->body, $emailData);
             $this->mail->setFrom($this->setting->email_from, $this->setting->email_from_name);
             $this->mail->clearAddresses();
             $this->mail->addAddress($this->setting->contact_email);
             $this->mail->isHTML(true);
             $this->mail->Subject = $template->subject;
             $this->mail->Body = $email_body;
-
-
-
             $this->mail->send();
+            Log::info('Admin order email sent.', $this->buildEmailLogContext($emailData, [
+                'template_type' => 'New Order Admin',
+                'recipient' => $this->setting->contact_email,
+            ]));
         } catch (\Throwable $th) {
-            dd($th->getMessage());
+            Log::error('Admin order email send failed.', $this->buildEmailLogContext($emailData, [
+                'template_type' => 'New Order Admin',
+                'recipient' => $this->setting->contact_email,
+                'error' => $th->getMessage(),
+            ]));
         }
+    }
+
+    protected function replaceTemplateTokens(string $body, array $emailData): string
+    {
+        $replacements = [
+            '{user_name}' => $emailData['user_name'] ?? '',
+            '{order_cost}' => $emailData['order_cost'] ?? '',
+            '{order_total}' => $emailData['order_cost'] ?? '',
+            '{transaction_number}' => $emailData['transaction_number'] ?? '',
+            '{order_number}' => $emailData['transaction_number'] ?? '',
+            '{site_title}' => $emailData['site_title'] ?? $this->setting->title,
+        ];
+
+        return strtr($body, $replacements);
+    }
+
+    protected function buildEmailLogContext(array $emailData, array $extra = []): array
+    {
+        $transactionNumber = $emailData['transaction_number'] ?? null;
+        $orderId = null;
+
+        if ($transactionNumber) {
+            $orderId = Order::where('transaction_number', $transactionNumber)->value('id');
+        }
+
+        return array_merge([
+            'order_id' => $orderId,
+            'transaction_number' => $transactionNumber,
+            'user_name' => $emailData['user_name'] ?? null,
+            'order_cost' => $emailData['order_cost'] ?? null,
+        ], $extra);
     }
 }
